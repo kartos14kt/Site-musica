894b32923faec3a8ef23be672cc852760f9b4d24
kartos14kt <muriloczr1515@gmail.com>
2026-03-09 12:00:02 -0400

    parei de usar iframes, e aprendi a colocar scrool vertical em divs

--- player.js
+++ player.js
@@ -1,9 +1,47 @@
-const nomesDivs = document.querySelectorAll('.nomes');
-nomesDivs.forEach(div => {
-    div.addEventListener('click', () => {
-        // Remove 'ativo' de todas
-        nomesDivs.forEach(d => d.classList.remove('ativo'));
-        // Adiciona 'ativo' na clicada
-        div.classList.add('ativo');
+function selecionarArtista(elemento){
+
+    // remove ativo dos artistas
+    const artistas = document.querySelectorAll(".nomes");
+    artistas.forEach(a => a.classList.remove("ativo"));
+
+    // adiciona ativo no clicado
+    elemento.classList.add("ativo");
+
+    // identifica qual artista foi clicado
+    const artista = elemento.dataset.artista;
+
+    // banco de músicas
+    const musicas = {
+        fabio: [
+            "Musica 1",
+            "Musica 2",
+            "Musica 3"
+        ],
+        joao: [
+            "Som A",
+            "Som B",
+            "Som C"
+        ]
+    };
+
+    const sobre = document.querySelector(".listM");
+
+    let html = "";
+
+    // cria as divs das músicas
+    musicas[artista].forEach(musica => {
+        html += `<div class="musica" onclick="selecionarMusica(this)">${musica}</div>`;
     });
-});
+
+    sobre.innerHTML = html;
+}
+
+
+// selecionar música
+function selecionarMusica(elemento){
+
+    const musicas = document.querySelectorAll(".musica");
+    musicas.forEach(m => m.classList.remove("ativo"));
+
+    elemento.classList.add("ativo");
+}
